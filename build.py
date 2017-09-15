f05a0955aa930ae528a0ee76e32118c5f4abac88
vinod-boga <vinodboga17@gmail.com>
2017-09-15 17:16:50 +0000

    Done

--- build.py
+++ build.py
@@ -2,13 +2,12 @@ import operator
 
 
 def solution_asc(dic):
-    '''
-    Enter your code here
-    '''
-
+    sorted_d = sorted(dic.items(), key=operator.itemgetter(0))
+    return sorted_d
 
 def solution_desc(dic):
-    '''
-    Enter your code here
-    '''
+    sorted_d = sorted(dic.items(), key=operator.itemgetter(0),reverse=True)
+    return sorted_d
 
+print solution_asc({1: 2, 3: 4, 4: 3, 2: 1, 0: 0})
+print solution_desc({1: 2, 3: 4, 4: 3, 2: 1, 0: 0})
